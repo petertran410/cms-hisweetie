2fee921f99d3971afce63d723aa914cdbe48b5ae
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-05-23 01:28:40 +0700

    Fixed category relationship

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,10 @@
+import { API } from '@/utils/API';
+import { showToast } from '@/utils/helper';
+import { tokenState, userInfoAtom } from '@/states/common';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+
 export const useMutateLogin = () => {
   const navigate = useNavigate();
   const setToken = useSetRecoilState(tokenState);
@@ -31,5 +38,26 @@ export const useMutateLogin = () => {
         message: 'Tài khoản hoặc mật khẩu không chính xác'
       });
     }
+  });
+};
+
+export const useQueryUserInfo = () => {
+  const setUserInfo = useSetRecoilState(userInfoAtom);
+
+  return useQuery({
+    queryKey: ['GET_USER_INFO'],
+    queryFn: async () => {
+      const response = await API.request({
+        url: '/api/user',
+        method: 'GET'
+      });
+
+      // Set user info in Recoil state
+      setUserInfo(response);
+
+      return response;
+    },
+    retry: false,
+    staleTime: 5 * 60 * 1000 // 5 minutes
   });
 };
