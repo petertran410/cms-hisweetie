d66812f810b3950d964ec8165f5db62766e9b9e0
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-09-27 12:32:50 +0700

    test CORS fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3333,
-    hmr: {
-      port: 3333
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
   },
-  define: {
-    global: 'globalThis'
+
+  server: {
+    port: 3333
   }
 });
