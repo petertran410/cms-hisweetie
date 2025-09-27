70baffb117ed89bcb6e41de47d6ae89cef5bcc69
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-09-27 12:20:56 +0700

    Change docker compose

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+  server: {
+    host: true,
+    port: 3333,
+    hmr: {
+      port: 3333
     }
   },
-
-  server: {
-    port: 3333
+  define: {
+    global: 'globalThis'
   }
 });
